501da7f3c55f3b16cc4fedd046efaa143659a286
Vishruti Patil <vishruti2908@gmail.com>
2023-03-16 15:45:03 +0530

    Binary tree created as given

--- src/Data_Structure/BinaryTree.java
+++ src/Data_Structure/BinaryTree.java
@@ -5,6 +5,30 @@ public class BinaryTree {
         bn.insert(56);
         bn.insert(30);
         bn.insert(70);
+        bn.insert(22);
+        bn.insert(40);
+        bn.insert(11);
+        bn.insert(3);
+        bn.insert(16);
+        bn.insert(60);
+        bn.insert(95);
+        bn.insert(65);
+        bn.insert(63);
+        bn.insert(67);
+
+        System.out.println(bn.root.value);
         System.out.println(bn.root.left.value);
+        System.out.println(bn.root.right.value);
+        System.out.println(bn.root.left.left.value);
+        System.out.println(bn.root.left.right.value);
+        System.out.println(bn.root.left.left.left.value);
+        System.out.println(bn.root.left.left.left.left.value);
+        System.out.println(bn.root.left.left.left.right.value);
+        System.out.println(bn.root.right.left.value);
+        System.out.println(bn.root.right.right.value);
+        System.out.println(bn.root.right.left.left.value);
+        System.out.println(bn.root.right.left.left.left.value);
+        System.out.println(bn.root.right.left.left.right.value);
+
     }
 }
